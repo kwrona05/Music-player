f9683cf30ee7ca7238404fbf47749aff3f172eaf
kwrona05 <kwrona05@gmail.com>
2024-09-23 07:44:21 +0200

    Add list of musics

---
+++ MusicApp/src/MusicList.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+
+function MusicList({ addToWatchList }) {
+  const [musics, setMusics] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://openwhyd.org/adrien");
+      setMusics(response.data || []);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError("Failed to fetch music data", error);
+      toast.error("Something went wrong during fetching music data", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleAdd = (music) => {
+    addToWatchList(music);
+    toast.success(`${music.name} added to your watchlist`);
+  };
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : musics?.length === 0 ? (
+        <p>No musics found</p>
+      ) : (
+        <ul>
+          {musics?.map((music) => (
+            <li key={music.id}>
+              {music.name}
+              <button onClick={() => handleAdd(music)}>Add to watchlist</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <ToastContainer />
+    </div>
+  );
+}
+export default MusicList;
